9fccb272aaa86f27cab66d1ea57c314f24708655
SamuelMartini <samueljmartini@gmail.com>
2020-07-17 16:11:48 +0200

    Convert TaxonsController spec to request

--- spec/requests/spree/taxons_controller_spec.rb
+++ spec/requests/spree/taxons_controller_spec.rb
@@ -2,13 +2,24 @@
 
 require 'spec_helper'
 
-describe Spree::TaxonsController, type: :controller do
-  it "should provide the current user to the searcher class" do
+describe Spree::TaxonsController, type: :request do
+  let(:user) { mock_model(Spree.user_class, has_spree_role?: 'admin', spree_api_key: 'fake') }
+  let(:searcher_class) { instance_double(Spree::Config.searcher_class) }
+
+  stub_spree_current_user
+
+  before do
+    allow(Spree::Config.searcher_class).to receive(:new) { searcher_class }
+    allow(searcher_class).to receive(:current_user=)
+    allow(searcher_class).to receive(:pricing_options=)
+    allow(searcher_class).to receive(:retrieve_products) { [] }
+  end
+
+  it "provides the current user to the searcher class" do
     taxon = create(:taxon, permalink: "test")
-    user = mock_model(Spree.user_class, last_incomplete_spree_order: nil, spree_api_key: 'fake')
-    allow(controller).to receive_messages try_spree_current_user: user
-    expect_any_instance_of(Spree::Config.searcher_class).to receive(:current_user=).with(user)
-    get :show, params: { id: taxon.permalink }
+    get spree.nested_taxons_path(taxon.permalink)
+
+    expect(searcher_class).to have_received(:current_user=).with(user)
     expect(response.status).to eq(200)
   end
 end
