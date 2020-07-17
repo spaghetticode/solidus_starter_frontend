646c788f8017561e328e905833a42a3a439cc177
SamuelMartini <samueljmartini@gmail.com>
2020-07-17 18:23:32 +0200

    Convert ProductsController spec to request

--- spec/requests/spree/products_controller_spec.rb
+++ spec/requests/spree/products_controller_spec.rb
@@ -2,37 +2,46 @@
 
 require 'spec_helper'
 
-describe Spree::ProductsController, type: :controller do
+describe Spree::ProductsController, type: :request do
   let!(:product) { create(:product, available_on: 1.year.from_now) }
+  let(:user) { mock_model(Spree.user_class) }
+  let(:searcher_class) { instance_double(Spree::Config.searcher_class) }
 
-  # Regression test for https://github.com/spree/spree/issues/1390
-  it "allows admins to view non-active products" do
-    allow(controller).to receive_messages try_spree_current_user: mock_model(Spree.user_class, has_spree_role?: true, last_incomplete_spree_order: nil, spree_api_key: 'fake')
-    get :show, params: { id: product.to_param }
-    expect(response.status).to eq(200)
+  stub_spree_current_user
+
+  before do
+    allow(Spree::Config.searcher_class).to receive(:new) { searcher_class }
+    allow(searcher_class).to receive(:current_user=)
+    allow(searcher_class).to receive(:pricing_options=)
+    allow(searcher_class).to receive(:retrieve_products) { [product] }
+  end
+
+  context 'as an admin' do
+    let(:user) { mock_model(Spree.user_class, has_spree_role?: 'admin', spree_api_key: 'fake') }
+
+    # Regression test for https://github.com/spree/spree/issues/1390
+    it "allows admins to view non-active products" do
+      get spree.product_path(product.to_param)
+      expect(response.status).to eq(200)
+    end
+
+    # Regression test for https://github.com/spree/spree/issues/2249
+    it "doesn't error when given an invalid referer" do
+      # Previously a URI::InvalidURIError exception was being thrown
+      get spree.product_path(product.to_param), headers: { 'HTTP_REFERER' => 'not|a$url' }
+    end
   end
 
   it "cannot view non-active products" do
-    expect {
-      get :show, params: { id: product.to_param }
-    }.to raise_error(ActiveRecord::RecordNotFound)
+    expect do
+      get spree.product_path(product.to_param)
+    end.to raise_error(ActiveRecord::RecordNotFound)
   end
 
   it "should provide the current user to the searcher class" do
-    user = mock_model(Spree.user_class, last_incomplete_spree_order: nil, spree_api_key: 'fake')
-    allow(controller).to receive_messages try_spree_current_user: user
-    expect_any_instance_of(Spree::Config.searcher_class).to receive(:current_user=).with(user)
-    get :index
+    get spree.products_path
+
+    expect(searcher_class).to have_received(:current_user=).with(user)
     expect(response.status).to eq(200)
-  end
-
-  # Regression test for https://github.com/spree/spree/issues/2249
-  it "doesn't error when given an invalid referer" do
-    current_user = mock_model(Spree.user_class, has_spree_role?: true, last_incomplete_spree_order: nil, generate_spree_api_key!: nil)
-    allow(controller).to receive_messages try_spree_current_user: current_user
-    request.env['HTTP_REFERER'] = "not|a$url"
-
-    # Previously a URI::InvalidURIError exception was being thrown
-    get :show, params: { id: product.to_param }
   end
 end
